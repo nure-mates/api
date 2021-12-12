7ab709cf99a477cf13750443257a17f3a9d70832
ynaumenko <ynaumenko@mirantis.com>
2021-12-12 22:22:01 +0200

    fixed

---
+++ src/storage/postgres/tracks.go
@@ -0,0 +1,24 @@
+package postgres
+
+import (
+	"context"
+
+	"github.com/nure-mates/api/src/models"
+)
+
+type TrackRepo struct {
+	*Postgres
+}
+
+func (p *Postgres) NewTrackRepo() *TrackRepo {
+	p.DB.RegisterModel((*models.Track)(nil))
+	return &TrackRepo{p}
+}
+
+func (r *TrackRepo) AddTrack(ctx context.Context, track *models.Track) error {
+	_, err := r.DB.NewInsert().
+		Model(track).
+		Returning("id").
+		Exec(ctx)
+	return err
+}
